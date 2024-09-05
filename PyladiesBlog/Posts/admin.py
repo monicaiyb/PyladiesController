7d5224cdcf37c65d4b185c92bf20faf0ef245c23
monicaiyb <monicaiyb@gmail.com>
2024-09-05 20:13:26 +0300

    Add views

--- PyladiesBlog/Posts/admin.py
+++ PyladiesBlog/Posts/admin.py
@@ -4,4 +4,4 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display=['title','author']
+    list_display=['title','author','slug','date_created','date_updated']
--- PyladiesBlog/Posts/models.py
+++ PyladiesBlog/Posts/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=30)
     content=models.CharField(max_length=30)
-    date_created=models.DateTimeField()
+    date_created=models.DateTimeField(auto_now=True)
+    date_updated=models.DateTimeField(auto_now=True)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
+    slug =models.SlugField(max_length=120,unique=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save (self,*args,**kwargs):
+        if not self.slug:
+            self.slug =slugify(self.title[:100] + str(now()))
+
+        super(self,Post).save(*args,**kwargs)
 
     def __str__(self):
         return self.title
